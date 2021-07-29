aedaff447a282d22e0cc3d04adef17c3d3455a0d
SanyiToth <sandor.toth@hotmail.com>
2021-07-29 20:41:13 +0200

    todos saved in an array

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import TodoList from "./components/TodoList";
 
 function App() {
     const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
     return (
         <div className="App">
             <header>
                 <h1>Sandor's Todo-List</h1>
             </header>
-            <Form setInputText={setInputText}/>
+            <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
             <TodoList/>
         </div>
     );
