642ba20e31863fdc03baef797e121915ec9f9c2c
Sanyi Toth <sandor.toth@hotmail.com>
2021-08-15 15:19:36 +0200

    completehandler works

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,16 +4,29 @@ import React from "react";
 const Todo = ({todo, todos, setTodos}) => {
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el?.id !== todo?.id))
-        console.log("todo", todo)
     }
+
+    const completeHandler = () => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === todo.id) {
+                    return {
+                        ...item, completed: !item.completed,
+                    }
+                }
+                return item;
+            })
+        );
+    };
+
     return (
         <div className="todo">
-            <li className="todo-item">{todo.text}</li>
-            <button className="complete-btn">
-                <i className="fas fa-check"></i>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{todo.text}</li>
+            <button onClick={completeHandler} className="complete-btn">
+                <i className="fas fa-check"/>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
+                <i className="fas fa-trash"/>
             </button>
         </div>
     );
